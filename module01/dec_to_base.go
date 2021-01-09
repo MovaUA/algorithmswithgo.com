756434081825bbd030ecd5eab31c3a6ac76485d2
Valeriy <valeriy.molchanov.77@gmail.com>
2021-01-09 21:54:19 +0200

    Features/dec to base (#4)
    * DecToBase: func (n digit) String() string
    
    * DecToBase: fmt.Sprintf("%X", r)
    
    * DecToBase: string index

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,5 +1,9 @@
 package module01
 
+import (
+	"strings"
+)
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -10,5 +14,14 @@ package module01
 //   DecToBase(14, 2) => "1110"
 //
 func DecToBase(dec, base int) string {
-	return ""
+	const s = "0123456789ABCDEF"
+	var sb strings.Builder
+	for {
+		r := dec % base
+		sb.WriteByte(s[r])
+		if dec /= base; dec == 0 {
+			break
+		}
+	}
+	return Reverse(sb.String())
 }
