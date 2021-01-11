b863261931c197c12ebe61a5a8fdc676c1963997
Valeriy <valeriy.molchanov.77@gmail.com>
2021-01-11 09:55:37 +0200

    BaseToDec (#5)
    * DecToBase: func (n digit) String() string
    
    * DecToBase: fmt.Sprintf("%X", r)
    
    * DecToBase: string index
    
    * BaseToDec: strconv.ParseInt(value, base, 64)
    
    * BaseToDec: parsing digit via map[rune]int

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,9 @@
 package module01
 
+import (
+	"math"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +13,29 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	m := map[rune]int{
+		'0': 0,
+		'1': 1,
+		'2': 2,
+		'3': 3,
+		'4': 4,
+		'5': 5,
+		'6': 6,
+		'7': 7,
+		'8': 8,
+		'9': 9,
+		'A': 10,
+		'B': 11,
+		'C': 12,
+		'D': 13,
+		'E': 14,
+		'F': 15,
+	}
+	const charset = "0123456789ABCDEF"
+	n := 0
+	l := len(value)
+	for i, v := range value {
+		n += m[v] * int(math.Pow(float64(base), float64(l-i-1)))
+	}
+	return n
 }
