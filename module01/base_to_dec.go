7e74aca2b11cbf9c3617b9fac8ad97ff66559860
mova <valeriy.molchanov.77@gmail.com>
2021-01-11 15:11:38 +0200

    BaseToDec: fmt.Sscanf

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -13,33 +13,14 @@ import (
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	m := map[byte]int{
-		'0': 0,
-		'1': 1,
-		'2': 2,
-		'3': 3,
-		'4': 4,
-		'5': 5,
-		'6': 6,
-		'7': 7,
-		'8': 8,
-		'9': 9,
-		'A': 10,
-		'B': 11,
-		'C': 12,
-		'D': 13,
-		'E': 14,
-		'F': 15,
-	}
 	n := 0
 	multiplier := 1
 	for i := len(value) - 1; i >= 0; i-- {
-		digit := value[i]
-		index, ok := m[digit]
-		if !ok {
-			panic(fmt.Sprintf("Unknown digit %q", digit))
+		var val int
+		if _, err := fmt.Sscanf(string(value[i]), "%X", &val); err != nil {
+			panic(fmt.Sprintf("Unexpected character %q at index %d: %v", value[i], i, err))
 		}
-		n += index * multiplier
+		n += val * multiplier
 		multiplier *= base
 	}
 	return n
