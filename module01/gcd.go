b86206909577d8f03c03b7c12fda836309971408
mova <valeriy.molchanov.77@gmail.com>
2021-01-11 23:43:04 +0200

    GCD: Euclidean algorythm

--- module01/gcd.go
+++ module01/gcd.go
@@ -2,46 +2,34 @@ package module01
 
 // GCD is the graitest common divider
 func GCD(a, b int) int {
+	if a == 0 {
+		return b
+	}
+	if b == 0 {
+		return a
+	}
 	if a == b {
 		return a
 	}
 
-	primes := []int{
-		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-		73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
-		179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-		283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
-		419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541,
-	}
+	min, max := minMax(a, b)
 
-	aFactor, bFactor := Factor(primes, a), Factor(primes, b)
-
-	aMap, bMap := make(map[int]int, len(aFactor)), make(map[int]int, len(bFactor))
-
-	for _, v := range aFactor {
-		aMap[v] = aMap[v] + 1
-	}
-	for _, v := range bFactor {
-		bMap[v] = bMap[v] + 1
-	}
-
-	res := 1
-
-	for _, prime := range primes {
-		aCount, aOk := aMap[prime]
-		bCount, bOk := bMap[prime]
-		if aOk && bOk {
-			res *= prime * min(aCount, bCount)
+	for {
+		r := max % min
+		if r == 0 {
+			return min
 		}
 
+		min, max = minMax(min, r)
+		if min == 1 {
+			return 1
+		}
 	}
-
-	return res
 }
 
-func min(a, b int) int {
+func minMax(a, b int) (int, int) {
 	if a < b {
-		return a
+		return a, b
 	}
-	return b
+	return b, a
 }
