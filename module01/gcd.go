15c40224acc36ee45bf805261153709c0ad0130c
mova <valeriy.molchanov.77@gmail.com>
2021-01-11 23:43:30 +0200

    GCD: Euclidean algorythm (refactored)

--- module01/gcd.go
+++ module01/gcd.go
@@ -2,28 +2,15 @@ package module01
 
 // GCD is the graitest common divider
 func GCD(a, b int) int {
-	if a == 0 {
-		return b
-	}
-	if b == 0 {
-		return a
-	}
-	if a == b {
-		return a
-	}
-
-	min, max := minMax(a, b)
-
+	a, b = minMax(a, b)
 	for {
-		r := max % min
-		if r == 0 {
-			return min
+		if a == 0 {
+			return b
 		}
-
-		min, max = minMax(min, r)
-		if min == 1 {
-			return 1
+		if b == 0 {
+			return a
 		}
+		a, b = minMax(b%a, a)
 	}
 }
 
