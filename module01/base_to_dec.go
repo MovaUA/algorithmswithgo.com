3171491194f0fbc9b56516f0832f9ae97626c82d
mova <valeriy.molchanov.77@gmail.com>
2021-01-11 09:53:56 +0200

    BaseToDec: parsing digit via map[rune]int

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,6 +1,8 @@
 package module01
 
-import "strconv"
+import (
+	"math"
+)
 
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
@@ -11,6 +13,29 @@ import "strconv"
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	n, _ := strconv.ParseInt(value, base, 64)
-	return int(n)
+	m := map[rune]int{
+		'0': 0,
+		'1': 1,
+		'2': 2,
+		'3': 3,
+		'4': 4,
+		'5': 5,
+		'6': 6,
+		'7': 7,
+		'8': 8,
+		'9': 9,
+		'A': 10,
+		'B': 11,
+		'C': 12,
+		'D': 13,
+		'E': 14,
+		'F': 15,
+	}
+	const charset = "0123456789ABCDEF"
+	n := 0
+	l := len(value)
+	for i, v := range value {
+		n += m[v] * int(math.Pow(float64(base), float64(l-i-1)))
+	}
+	return n
 }
