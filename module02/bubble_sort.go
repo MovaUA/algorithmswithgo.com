a36969c1e8d0ed3cfd74266629c45a700ea8465a
mova <valeriy.molchanov.77@gmail.com>
2021-01-12 22:54:42 +0200

    BubbleSortString

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -23,6 +23,18 @@ func BubbleSortInt(list []int) {
 // BubbleSortString is a bubble sort for string slices. Try implementing it for
 // practice.
 func BubbleSortString(list []string) {
+	for sweepNum := 0; sweepNum < len(list)-1; sweepNum++ {
+		swapped := false
+		for i := 0; i < len(list)-1-sweepNum; i++ {
+			if list[i] > list[i+1] {
+				list[i], list[i+1] = list[i+1], list[i]
+				swapped = true
+			}
+		}
+		if !swapped {
+			return
+		}
+	}
 }
 
 // BubbleSortPerson uses bubble sort to sort Person slices by: Age, then
